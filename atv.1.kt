eece82cc94430638dabbdf1e5a2d4c3c9b339a4d
krebin0705 <148490151+krebin0705@users.noreply.github.com>
2025-04-09 12:15:14 -0300

    Update atv.1.kt

--- atv.1.kt
+++ atv.1.kt
@@ -1,1 +1,8 @@
 
+fun main() {
+    for (i in 30 downTo 0) {
+        println("Contagem: $i")
+        Thread.sleep(1000) // Pausa de 1 segundo (1000 milissegundos)
+    }
+    println("💥 EXPLOSÃO 💥")
+}
